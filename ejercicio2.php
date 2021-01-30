65175d4830219b4eb176e5fe659cf2ad3623c462
fullstack82 <fullstack8210@gmail.com>
2021-01-31 00:40:19 +0100

    ejercicio acabado

--- ejercicio2.php
+++ ejercicio2.php
@@ -9,5 +9,8 @@ longitud sea menor a 120 y luego mostrarlo por pantalla;
 $coleccion = array();
 
 for($i = 0; $i <= 120; $i++){
-
+        array_push($coleccion,"elemento-".$i);
 };
+
+
+echo $coleccion[115];
