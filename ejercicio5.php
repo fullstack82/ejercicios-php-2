1075d96bd0e2997edfac5fbe26ce325d1a1ce700
fullstack82 <fullstack8210@gmail.com>
2021-01-31 14:52:33 +0100

    realizando el ejercicio numero 5

--- ejercicio5.php
+++ ejercicio5.php
@@ -1,7 +1,18 @@
 <?php 
 /*
-
-
-
-
+*   Crear un array con el contenido de la siguiente tabla:
+* - accion : GTA, COD, FORNITE
+* - aventura: ASSASINS, CRASH, PERSIA
+* - Deportes: FIFA, PES, MOTTO GP
+* 
+* Cada columna debe ir en un fichero separado.
+*
 */
+
+
+$tabla = array(                                            // Array multidimensional
+    "ACCION" => array("GTA", "COD", "FORNITE"),
+    "AVENTURA" => array("ASSASINS", "CRASH", "PERSIA"),
+    "DEPORTES" => array("FIFA", "PES", "MOTO GP")
+);
+
